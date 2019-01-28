3024745d519c71ef7510e6c7f73265594a7c5358
Max Helmetag <mhelmetag@gmail.com>
2019-01-27 22:44:34 -0800

    fiddly boonli magic, it works

--- src/content.js
+++ src/content.js
@@ -1,1 +1,19 @@
-(function() {})();
+(function() {
+  const xpathResults = document.evaluate(
+    '//label[text()=\'Non-card Payment\']',
+    document
+  );
+  const nonCardLabel = xpathResults.iterateNext();
+
+  if (nonCardLabel) {
+    const nonCardRadio = nonCardLabel.parentElement.firstElementChild;
+
+    nonCardRadio.addEventListener('click', () => {
+      const nonCardDetailsTextBox = document.querySelector(
+        'div[id=\'non-card-payment\']>input'
+      );
+
+      nonCardDetailsTextBox.value = 'Payroll Deduction';
+    });
+  }
+})();
